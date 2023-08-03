9fd3ca53629b16bb70d653a41d10b9e116eb9f9f
Вікторія <viktoriya23071@gmail.com>
2023-08-03 23:28:26 +0300

    uppdata-01

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,29 @@
 
+
+const dataStartRef = document.querySelector('[data-start]');
+const dataStopRef = document.querySelector('[data-stop]');
+const bodyRef = document.querySelector('body');
+
+let timerId = null;
+
+dataStartRef.addEventListener('click', onStart);
+dataStoptRef.addEventListener('click', onStop);
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+}
+
+function onStart() {
+  timerId = setInterval(getBgColor, 1000);
+  dataStartRef.toggleAttribute('disable');
+}
+
+function onStop() {
+  clearInterval(timerId);
+  dataStopRef.removeAttribute('disable');
+}
+
+function getBgColor() {
+  bodyRef.style.backgroundColor = getRandomHexColor();
+}
+
